d05a42da67cadf870bf20605372c218a4217ed56
Sharpieeee14 <93734217+Sharpieeee14@users.noreply.github.com>
2022-03-14 15:15:38 -0700

    Add files via upload

---
+++ sspw-released/mail/contact.php
@@ -0,0 +1,20 @@
+<?php
+if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+  http_response_code(500);
+  exit();
+}
+
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email = strip_tags(htmlspecialchars($_POST['email']));
+$m_subject = strip_tags(htmlspecialchars($_POST['subject']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+
+$to = "nsharpenshine@gmail.com"; // Change this email to your //
+$subject = "$name | SSPW";
+$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\Number: $m_subject\n\nMessage: $message";
+$header = "From: $email";
+$header .= "Reply-To: $email";	
+
+if(!mail($to, $subject, $body, $header))
+  http_response_code(500);
+?>
